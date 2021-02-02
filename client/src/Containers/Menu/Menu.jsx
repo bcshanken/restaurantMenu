3f5a9184ece081830e42a2630d7cb7461857d616
Rocco <roccomaniscalco2001@gmail.com>
2021-02-01 23:14:51 -0500

    styled MenuItem component

--- client/src/Containers/Menu/Menu.jsx
+++ client/src/Containers/Menu/Menu.jsx
@@ -7,18 +7,38 @@ const Menu = () => {
       _id: 123,
       title: "Salmon",
       description:
-        "Blackened salmon served with peppers, grilled onions, and white rice",
+        "Blackened salmon served with peppers, grilled onions, and white rice.",
       img:
         "https://www.dinneratthezoo.com/wp-content/uploads/2019/06/broiled-salmon-11-500x500.jpg",
       price: 19.99,
       category: "entrees",
     },
+    {
+      _id: 124,
+      title: "Salmon",
+      description:
+        "Blackened salmon served with peppers, grilled onions, and white rice.",
+      img:
+        "https://www.dinneratthezoo.com/wp-content/uploads/2019/06/broiled-salmon-11-500x500.jpg",
+      price: 19.99,
+      category: "entrees",
+    },
+    {
+        _id: 125,
+        title: "Salmon",
+        description:
+          "Blackened salmon served with peppers, grilled onions, and white rice.",
+        img:
+          "https://www.dinneratthezoo.com/wp-content/uploads/2019/06/broiled-salmon-11-500x500.jpg",
+        price: 19.99,
+        category: "entrees",
+      },
   ];
 
   return (
     <div>
       {menuItems.map((menuItem) => (
-        <MenuItem {...menuItem} />
+        <MenuItem {...menuItem} key={menuItem._id} />
       ))}
     </div>
   );
