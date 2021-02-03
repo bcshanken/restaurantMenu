fb9a9dd365bc6726092a3b94c77d66ac165adc66
Dame <dcampbell7095@gmail.com>
2021-02-02 22:05:01 -0500

    adding basic scaffold of orders container mapping through array of objects

--- client/src/Containers/Orders/Orders.jsx
+++ client/src/Containers/Orders/Orders.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import MenuItem from "../../Components/Navbar/MenuItem.jsx/MenuItem";
 
 const Orders = () => {
-    return (
-        <div>
-            orders
-        </div>
-    );
+  const orders = [
+    {
+      title: "#001",
+      content: [
+        {
+          imageUrl: "http://placehold.it/300x300",
+          title: "Salmon",
+          price: 19.99,
+        },
+        {
+          imageUrl: "http://placehold.it/300x300",
+          title: "Jerk Chicken Pasta",
+          price: 19.99,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      {orders.map((orderItems, index) => {
+          <h1>{orderItems.title}</h1>
+        return (
+          <div className="card horizontal">
+              <div className="card-stacked">
+            {orderItems.content.map((order) => {
+              return (
+                <>
+                    <div className="card-content">
+                      <span className="card-title">{order.title}</span>
+                      <strong>{order.price}</strong>
+                    </div>
+                </>
+              );
+            })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Orders;
