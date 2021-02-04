b79dddcbc3ef361bf13aa30aac9197710221de69
Dame <dcampbell7095@gmail.com>
2021-02-03 21:12:06 -0500

    adding styling to OrderCard element

--- client/src/Components/OrderCard/OrderCard.jsx
+++ client/src/Components/OrderCard/OrderCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const OrderCard = ({title, price}) => {
   return (
-    <div className="card-content order-card">
+    <div className="card-content">
       <span className="card-title">{title}</span>
       <strong>{price}</strong>
     </div>
