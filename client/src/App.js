3f204c79fe2de594137345f2d505fb8485f57fd2
bcshanken <bcshanken@gmail.com>
2021-02-08 19:29:01 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import AdminMenu from './Containers/AdminMenu/AdminMenu';
 import NewFood from './Containers/NewFood/NewFood';
 import Home from './Containers/Home/Home';
 import Login from './Containers/Login/Login'
+import UpdateFood from './Containers/UpdateFood/UpdateFood';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route exact path="/adminmenu" component={ AdminMenu} />
         <Route exact path="/newfood" component={ NewFood} />
         <Route exact path="/adminlogin" component={Login}/>
+        <Route exact path="/updatefood/:id" component={UpdateFood}/>
       </Switch>
     </Router>
   );
--- client/src/Components/Navbar/NavAdmin.jsx
+++ client/src/Components/Navbar/NavAdmin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Navbar/Nav.css";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 const NavAdmin = () => {
   return (
