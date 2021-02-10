a68acdbe048599f0929f396e73d34b21f0ae5424
Dame <dcampbell7095@gmail.com>
2021-02-09 22:06:20 -0500

    adding form to register user

---
+++ client/src/Containers/NewUser/NewUser.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import NavAdmin from "../../Components/Navbar/NavAdmin"
+
+const NewUser = () => {
+    return (
+        <div className="container login-card">
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">Admin Login</span>
+                <div className="row">
+                  <form className="col s12" onSubmit={handleFormSubmit}>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <input
+                          id="email"
+                          type="email"
+                          className="validate"
+                          value={email}
+                          onChange={(e) => {
+                            setEmail(e.target.value);
+                          }}
+                        />
+                        <label htmlFor="email">Email</label>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-field col s12">
+                        <input
+                          id="password"
+                          type="password"
+                          className="validate"
+                          value={password}
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                        />
+                        <label htmlFor="password">Password</label>
+                      </div>
+                      <button
+                        className="btn waves-effect waves-light submit-button"
+                        type="submit"
+                        name="action"
+                      >
+                        Sign In
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default NewUser;
