1a3533bc5d32364e9036902bd49ceb622d4d4519
bcshanken <bcshanken@gmail.com>
2021-02-10 10:37:12 -0500

    mostly just adding some styling

--- client/src/Containers/AdminMenu/AdminMenu.jsx
+++ client/src/Containers/AdminMenu/AdminMenu.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import API from "../../utils/API";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 import NavAdmin from "../../Components/Navbar/NavAdmin";
+import "./AdminMenu.css";
+import MenuRow from "../../Components/MenuRow/MenuRow";
 
 const AdminMenu = () => {
   const [menuItems, setMenuItems] = useState([]);
@@ -24,76 +25,66 @@ const AdminMenu = () => {
   };
 
   const handleDelete = (id) => {
-   console.log(id)
-   API.deleteResponse(id)
-   .then(res => initializeMenu())
-   .catch(err => console.log(err));
+    console.log(id);
+    API.deleteResponse(id)
+      .then((res) => initializeMenu())
+      .catch((err) => console.log(err));
   };
 
- 
   return (
     <>
-    <NavAdmin/>
+      <NavAdmin />
       <div className="container">
+        <div className="adminMenu">
           <div className="row">
-              <div className="col s12">
-                  <h1 className="center-align">Admin Menu</h1>
-              </div>
-              <div className="col s12">
-                <Link to="/newfood">
-                  <button className="btn waves-effect waves-light submit-button">Create New Food</button>
-                </Link>
-              </div>
+            <div className="col s12">
+              <h1 className="center-align">Admin Menu</h1>
+            </div>
+            <div className="col s12">
+              <Link to="/newfood">
+                <button className="btn waves-effect waves-light submit-button">
+                  Create New Food
+                </button>
+              </Link>
+            </div>
           </div>
           <div className="row">
-              <div classname="col s12">
+            <div classname="col s12">
               <table>
-        <thead>
-          <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Img</th>
-              <th>Category</th>
-              <th>Price</th>
-              <th>Edit</th>
-              <th>Delete</th>
-          </tr>
-        </thead>
-
-        <tbody>
-        {menuItems.map(
-                ({
-                  _id,
-                  title,
-                  description,
-                  price,
-                  img,
-                  category,                  
-                }) => (
-                  <tr key={_id}>
-                    <td>{title}</td>
-                    <td>{description}</td>
-                    <td><img
-                        src={img}
-                        alt={title}
-                        style={{ height: "3em" }}
-                      ></img>
-</td>
-                    <td>{category}</td>
-                    <td>${price}</td>
-                    <td><Link to={`/updatefood/${_id}`}><FontAwesomeIcon icon={faEdit} /></Link>
-                    </td>
-                    <td><FontAwesomeIcon icon={faTrash} onClick={() => handleDelete(_id)} /></td>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Img</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                   </tr>
-                )
-              )}
-        </tbody>
-      </table>
-              </div>
+                </thead>
+
+                <tbody>
+                  {menuItems.map(
+                    ({ _id, title, description, price, img, category }) => (
+                      <MenuRow
+                        _id={_id}
+                        title={title}
+                        description={description}
+                        price={price}
+                        img={img}
+                        category={category}
+                        handleDelete={handleDelete}
+                      />
+                    )
+                  )}
+                </tbody>
+              </table>
+            </div>
           </div>
+        </div>
       </div>
-      </>
-  )
+    </>
+  );
 };
 
 export default AdminMenu;
