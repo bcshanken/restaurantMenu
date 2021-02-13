4e7320e963d701daa430fd42ed5588b9d67c7d75
Rocco <roccomaniscalco2001@gmail.com>
2021-02-13 11:49:56 -0500

    created empty cart page

--- client/src/Containers/Checkout/Checkout.jsx
+++ client/src/Containers/Checkout/Checkout.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import CheckoutItem from "../../Components/CheckoutItem/CheckoutItem";
 import UserNav from "../../Components/UserNav.jsx/UserNav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Checkout.css";
 
 const Checkout = () => {
   const [orderItems, setOrderItems] = useState(
@@ -8,11 +11,20 @@ const Checkout = () => {
   );
 
   return (
-    <div>
+    <div className="checkout-wrapper">
       <UserNav />
-      {orderItems.map((orderItem) => (
-        <CheckoutItem {...orderItem} key={orderItem.createdAt} />
-      ))}
+      {orderItems.length === 0 ? (
+        <div id="message-wrapper">
+          <div id="circle">
+            <FontAwesomeIcon icon={faShoppingCart} size="8x" id="cart-icon" />
+          </div>
+          <strong id="empty-cart-message">Your cart is empty</strong>
+        </div>
+      ) : (
+        orderItems.map((orderItem) => (
+          <CheckoutItem {...orderItem} key={orderItem.createdAt} />
+        ))
+      )}
     </div>
   );
 };
