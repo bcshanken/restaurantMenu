2cded97de4f0d597903078c87d9484b8a5629a9a
bcshanken <bcshanken@gmail.com>
2021-02-13 13:54:32 -0500

    added in filter and sort fucntinality

--- client/src/Components/MenuRow/MenuRow.jsx
+++ client/src/Components/MenuRow/MenuRow.jsx
@@ -10,7 +10,7 @@ const MenuRow = ({ title, description, img, price, _id, category, handleDelete})
     return (
         <tr key={_id}>
                     <td>{title}</td>
-                    <td class="menuDesc">{description}</td>
+                    <td className="menuDesc">{description}</td>
                     <td><img
                         src={img}
                         alt={title}
@@ -19,9 +19,9 @@ const MenuRow = ({ title, description, img, price, _id, category, handleDelete})
 </td>
                     <td>{category}</td>
                     <td>${price}</td>
-                    <td  class="menuIcon"><Link to={`/updatefood/${_id}`}><FontAwesomeIcon icon={faEdit} className="iconlink"/></Link>
+                    <td  className="menuIcon"><Link to={`/updatefood/${_id}`}><FontAwesomeIcon icon={faEdit} className="iconlink"/></Link>
                     </td>
-                    <td  class="menuIcon"><FontAwesomeIcon icon={faTrash}  onClick={() => handleDelete(_id)} /></td>
+                    <td  className="menuIcon"><FontAwesomeIcon icon={faTrash}  onClick={() => handleDelete(_id)} /></td>
                   </tr>
     );
 };
