774be0bac056311c2fce09f9ae6851d451883ce8
Dame <dcampbell7095@gmail.com>
2021-02-13 12:54:34 -0500

    adding hardcoded values for orders card

--- client/src/Components/OrderCard/OrderCard.jsx
+++ client/src/Components/OrderCard/OrderCard.jsx
@@ -1,14 +1,37 @@
 import React from "react";
+import "../OrderCard/OrderCard.css"
 
 const OrderCard = ({ title, price, desc, details }) => {
-
-
   return (
-    <div className="card-content">
-      {title.map((newTitle) => (
-        <span className="card-title">{newTitle}</span>
-      ))}
-      <strong className="card-title orders-strong">{price}</strong>
+    <div className="card-content orders-card-wrapper">
+      <div className="col s6 input-field">
+        <select
+          className="browser-default"
+          name="category"
+          // value={category}
+          // onChange={(e) => {
+          //   setCategory(e.target.value);
+          // }}
+        >
+          <option value="" selected>
+            Status
+          </option>
+          <option value="Pending">Pending</option>
+          <option value="Cooking">Cooking</option>
+          <option value="Finished">Finished</option>
+          <option value="Sent">Sent</option>
+        </select>
+      </div>
+      {title.map((newTitle) => {
+        return (
+          <>
+            <span className="card-title orders-title">{newTitle}</span>
+            <span className="card-title orders-detail">{details}</span>
+          </>
+        );
+      })}
+
+      <strong className="card-title orders-text">Subtotoal: $28.99</strong>
     </div>
   );
 };
--- client/src/Containers/Orders/Orders.jsx
+++ client/src/Containers/Orders/Orders.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AdminNav from "../../Components/AdminNav.jsx/AdminNav";
-import OrderCard from "../../Components/OrderCard/OrderCard"
-import "../Orders/Orders.css"
+import OrderCard from "../../Components/OrderCard/OrderCard";
+import "../Orders/Orders.css";
 
 const Orders = () => {
   const orders = [
@@ -9,12 +9,11 @@ const Orders = () => {
       orderID: "001",
       items: [
         {
-          title: ["fish", " tacos"],
-          description: "tacos",
-          details: "Good",
+          title: ["Fish", " Tacos"],
+          description: ["fish", "tacos"],
+          details: ["Good", " pretty good"],
           price: ["3.99", " 25.00"],
         },
-        
       ],
       status: "Cooking",
     },
@@ -23,56 +22,37 @@ const Orders = () => {
       orderID: "002",
       items: [
         {
-          title: ["tacos", "fish"],
+          title: ["Pizza", "Hamburger"],
           description: "fish",
-          details: "fishy fish",
+          details: ["pizzaDeets", " HamburgerDeets"],
           price: ["25.00", " 3.99"],
         },
       ],
       status: "Cooking",
-    }
-  ]
+    },
+  ];
 
   return (
     <>
-    <AdminNav/>
-    <div className="order-wrapper">
-      {orders.map((orderItems, index) => {
-          <h1>{orderItems.orderID}</h1>
-        return (
-          <div className="card horizontal order-category-wrapper order-card">
-            
+      <AdminNav />
+      <div className="order-wrapper">
+        {orders.map((orderItems, index) => {
+          <h1>{orderItems.orderID}</h1>;
+          return (
+            <div className="card horizontal order-category-wrapper order-card">
               <div className="card-stacked">
-              <div className="col s3 input-field">
-                <select
-                  className="browser-default"
-                  name="category"
-                  // value={category}
-                  // onChange={(e) => {
-                  //   setCategory(e.target.value);
-                  // }}
-                >
-                  <option value="" selected>
-                    Category
-                  </option>
-                  <option value="Pending">Appetizer</option>
-                  <option value="Cooking">Entree</option>
-                  <option value="Finished">Dessert</option>
-                  <option value="Sent">Side</option>
-                </select>
+                {orderItems.items.map((order) => {
+                  return (
+                    <>
+                      <OrderCard {...order} />
+                    </>
+                  );
+                })}
               </div>
-            {orderItems.items.map((order) => {
-              return (
-                <>
-                    <OrderCard {...order}/>
-                </>
-              );
-            })}
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
     </>
   );
 };
