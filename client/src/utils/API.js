ca3f4a7e61fccfe0d489910ff995e86b749a2e28
Rocco <roccomaniscalco2001@gmail.com>
2021-02-16 19:24:47 -0500

    refactored order model

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,27 +2,25 @@ import axios from "axios";
 
 // eslint-disable-next-line
 export default {
-  // Gets all Menu Items
   getMenu: function() {
     return axios.get("/api/menu");
   },
-  // Gets the menu item with the given id
   getItem: function(id) {
     return axios.get("/api/menu/" + id);
   },
-  // Deletes the book with the given id
   deleteResponse: function(id) {
     return axios.delete("/api/menu/" + id);
   },
-  // Saves a book to the database
   createResponse: function(responseData) {
     return axios.post("/api/menu", responseData);
   },
-  // Updates a book in the database
   updateResponse: function(responseData, id) {
     return axios.put("/api/menu/" + id, responseData);
   },
   getUsers: function(email) {
     return axios.post("/api/user/" + email)
+  },
+  createOrder: function(order) {
+    return axios.post("/api/order", order)
   }
 };
--- models/order.js
+++ models/order.js
@@ -1,23 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const autoIncrement = require('mongoose-auto-increment');
+const autoIncrement = require("mongoose-auto-increment");
 
 // auto increment middleware
 autoIncrement.initialize(mongoose.connection);
 
 const orderSchema = new Schema({
-  orderID: { type: Schema.Types.ObjectId, required: true},
-  items: [{
-    title:{ type: String, required: true},
-    desc:{ type: String, required: true},
-    details: { type: String, required: true},
-    price: { type: String, required: true},
-  }],
-  status: { type: String, required: true, enum: ["pending", "cooking", "finished", "sent"], default: "pending"},
-  date: { type: Date, default: Date.now }
+  items: [
+    {
+      menuItem: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Menu",
+        required: true,
+      },
+      addOns: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Menu",
+        },
+      ],
+      instructions: { type: String },
+    },
+  ],
+  status: {
+    type: String,
+    required: true,
+    enum: ["pending", "cooking", "finished", "sent"],
+    default: "pending",
+  },
+  date: { type: Date, default: Date.now },
+  price: { type: String, required: true },
 });
 
-orderSchema.plugin(autoIncrement.plugin, 'Order');
+orderSchema.plugin(autoIncrement.plugin, {
+  model: "Order",
+  startAt: 1,
+});
+
 const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
