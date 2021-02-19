ce8ed94692d369e1efa843e9d2fb83716973d0e2
Dame <dcampbell7095@gmail.com>
2021-02-19 14:59:44 -0500

    adding descriptive list to style orders card

--- client/src/Components/OrderCard/OrderCard.jsx
+++ client/src/Components/OrderCard/OrderCard.jsx
@@ -4,17 +4,20 @@ import "../OrderCard/OrderCard.css";
 const OrderCard = ({ menuItem, details, addOns, instructions,  }) => {
   return (
     <div className="card-content orders-card-wrapper">
-      <span className="card-title orders-title">{menuItem.title}</span>
-      <span className="card-title">Add-Ons:</span>
+      {/* <span className="card-title orders-title">{menuItem.title}</span> */}
+<dl>
+  <dt><span className="card-title orders-title">{menuItem.title}</span></dt>
+  <span className="card-title">Add-Ons:</span>
       {addOns.map((addOn) => {
         return (
           <React.Fragment>
-            <div>
-              <span className="card-title order-add-ons">{addOn.title}</span>
-            </div>
+                <dd>
+                  - {addOn.title}
+                </dd>
           </React.Fragment>
         );
       })}
+</dl>
       <span className="card-title">Customer Instructions: {instructions}</span>
     </div>
   );
