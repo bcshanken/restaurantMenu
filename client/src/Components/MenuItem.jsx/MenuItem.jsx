80aac90a5fde9e02585edeba6e3c6dbc89fac384
Rocco <roccomaniscalco2001@gmail.com>
2021-02-22 21:54:32 -0500

    fixed line-clamping on safari

--- client/src/Components/MenuItem.jsx/MenuItem.jsx
+++ client/src/Components/MenuItem.jsx/MenuItem.jsx
@@ -5,14 +5,13 @@ const MenuItem = ({ title, description, img, price, category, _id }) => {
   return (
     <div className="card horizontal menu-item-card">
       <img className="menu-item-image" src={img} alt={title} />
-      <div className="card-stacked">
-        <div className="card-content menu-item-content">
-          <p className="menu-item-description">
-            <span className="card-title menu-item-title">{title}</span>
-            {description}
-          </p>
-          <strong className="menu-item-price">{price}</strong>
+      <div className="menu-item-content">
+        <div className="line-clamp-4">
+          <h1 className="menu-item-title">{title}</h1>
+          <br className="inline-spacer" />
+          <p className="menu-item-description">{description}</p>
         </div>
+        <strong className="menu-item-price">{price}</strong>
       </div>
     </div>
   );
